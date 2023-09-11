fc3e016a15164f5fe96561d3cb274621891c76a7
lingnath <32849838+lingnath@users.noreply.github.com>
2023-09-11 14:24:54 -0700

    Add files via upload

---
+++ create_docker_containers.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# ONLY run this once. The next time you want to start these containers, please insert "docker start <container name>" instead
+
+# Create mysql container
+docker run -dit --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=debezium -e MYSQL_USER=mysqluser -e MYSQL_PASSWORD=mysqlpw debezium/example-mysql:1.6
+# Set up mysql container
+chmod +x set_up_mysql.sh
+docker cp set_up_mysql.sh mysql:/set_up_mysql.sh
+docker exec mysql ./set_up_mysql.sh
+# Create nifi container
+docker run --name nifi -p 8080:8080 -p 8443:8443 --link mysql:mysql -d apache/nifi:1.12.0
+# Set up nifi container
+chmod +x set_up_nifi.sh
+docker cp set_up_nifi.sh nifi:/opt/nifi/nifi-current/set_up_nifi.sh
+docker exec nifi ./set_up_nifi.sh
+# Create zookeeper container
+docker run -dit --name zookeeper -p 2181:2181 -p 2888:2888 -p 3888:3888 debezium/zookeeper:1.6
+# Create kafka container
+docker run -dit --name kafka -p 9092:9092 --link zookeeper:zookeeper debezium/kafka:1.6
+# Create debezium connect container
+docker run -dit --name connect -p 8083:8083 -e GROUP_ID=1 -e CONFIG_STORAGE_TOPIC=my-connect-configs -e OFFSET_STORAGE_TOPIC=my-connect-offsets -e STATUS_STORAGE_TOPIC=my_connect_statuses --link zookeeper:zookeeper --link kafka:kafka --link mysql:mysql debezium/connect:1.6
+# Create mysql debezium connector for Kafka
+curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ -d '{ "name": "inventory-connector", "config": { "connector.class": "io.debezium.connector.mysql.MySqlConnector", "tasks.max": "1", "database.hostname": "mysql", "database.port": "3306", "database.user": "debezium", "database.password": "dbz", "database.server.id": "184054", "database.server.name": "dbserver1", "database.include.list": "demo", "database.history.kafka.bootstrap.servers": "kafka:9092", "database.history.kafka.topic": "dbhistory.demo" } }'
+# Create superset container
+docker pull stantaov/superset-athena:0.0.1
+# Setting the port to be 8088 so that it doesn't conflict with port 8080 that Airflow runs on
+docker run -d -p 8088:8088 --name superset stantaov/superset-athena:0.0.1
+docker exec -it superset superset fab create-admin \
+               --username admin \
+               --firstname Superset \
+               --lastname Admin \
+               --email admin@superset.com \
+               --password admin
+docker exec -it superset superset db upgrade
+docker exec -it superset superset init
---
+++ set_up_nifi.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir custom-jars
+cd custom-jars
+wget http://java2s.com/Code/JarDownload/mysql/mysql-connector-java-5.1.17-bin.jar.zip
+unzip mysql-connector-java-5.1.17-bin.jar.zip
