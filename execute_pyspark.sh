c54849fa1feb55c4c3b1c5af89d6ab16019b2868
lingnath <32849838+lingnath@users.noreply.github.com>
2023-09-11 14:53:48 -0700

    Update execute_pyspark.sh

--- execute_pyspark.sh
+++ execute_pyspark.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd spark-3.3.3-bin-hadoop3/bin
-./spark-submit --master local[*] --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.3,org.apache.hudi:hudi-spark3-bundle_2.12:0.12.3 --conf "spark.serializer=org.apache.spark.serializer.KryoSerializer" /home/ubuntu/de_final_project/pyspark_streaming.py
+./spark-submit --master local[*] --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.3,org.apache.hudi:hudi-spark3-bundle_2.12:0.12.3 --conf "spark.serializer=org.apache.spark.serializer.KryoSerializer" ../../pyspark_streaming.py
