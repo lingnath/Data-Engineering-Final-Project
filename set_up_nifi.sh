e097694cfcb388ff75447a8920d1f5aa72465cdf
lingnath <32849838+lingnath@users.noreply.github.com>
2023-09-12 15:23:30 -0700

    Update set_up_nifi.sh

--- set_up_nifi.sh
+++ set_up_nifi.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# Install the jar file inside of the nifi container
 mkdir custom-jars
 cd custom-jars
 wget http://java2s.com/Code/JarDownload/mysql/mysql-connector-java-5.1.17-bin.jar.zip
