1eaf0ae7ee68aa50687c739a5f47e0436270657e
lingnath <32849838+lingnath@users.noreply.github.com>
2023-09-12 15:19:08 -0700

    Update execute_pyspark.sh

--- execute_pyspark.sh
+++ execute_pyspark.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+# Executing the pyspark streaming script. Please press Ctrl+C in the terminal to end this script. This script was designed to run indefinitely.
 cd spark-3.3.3-bin-hadoop3/bin
 ./spark-submit --master local[*] --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.3,org.apache.hudi:hudi-spark3-bundle_2.12:0.12.3 --conf "spark.serializer=org.apache.spark.serializer.KryoSerializer" ../../pyspark_streaming.py
