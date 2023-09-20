b1f3dd9476c93d6a89f91b711d3a5e82670a8d92
lingnath <32849838+lingnath@users.noreply.github.com>
2023-09-20 15:48:59 -0700

    Update install_packages.sh

--- Software_Installations/install_packages.sh
+++ Software_Installations/install_packages.sh
@@ -4,6 +4,7 @@
 sudo apt-get update
 sudo apt-get install awscli -y
 sudo apt-get install pip -y
+sudo apt-get install amazon-ec2-utils -y
 pip3 install boto3
 pip3 install python_dotenv
 pip3 install toml
