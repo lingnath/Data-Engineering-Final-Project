9c2fda654de421796e94cefc78d9c3ffbcd8cd1e
lingnath <32849838+lingnath@users.noreply.github.com>
2023-09-27 19:50:45 -0700

    Update dedup_bus_status_table.sql

--- dedup_bus_status_table.sql
+++ dedup_bus_status_table.sql
@@ -1,6 +1,6 @@
 -- Please enter this line of code into your superset and then save it as a virtual dataset so that you can start building dashboards from this deduplicated data
 select * from
 (select *, row_number() over (partition by record_id) as dedup 
-from awsdatacatalog.{enter athena db name}.bus_status) A
+from awsdatacatalog.automated_de_final_nl.bus_status) A
 where dedup=1
 order by record_id
