4d4986114e7d35b76daf967baec8825155d12cc5
lingnath <32849838+lingnath@users.noreply.github.com>
2023-09-30 07:06:11 -0700

    Update install_pyspark.sh

--- Software_Installations/install_pyspark.sh
+++ Software_Installations/install_pyspark.sh
@@ -10,7 +10,7 @@ sudo apt-get install default-jdk -y
 wget https://dlcdn.apache.org/spark/spark-3.3.3/spark-3.3.3-bin-hadoop3.tgz
 # Unzip the Spark package
 tar -xvf spark-3.3.3-bin-hadoop3.tgz
-# Download the jar files needed in order to read s3 files from the pyspark cli
+# Download the jar files needed in order to read S3 files from the pyspark cli
 cd spark-3.3.3-bin-hadoop3/jars
 wget https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-aws/3.3.3/hadoop-aws-3.3.3.jar
 wget https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-bundle/1.12.451/aws-java-sdk-bundle-1.12.451.jar
